8e72897318a412b3383619c3ad3c1dd5624f9b6f
Phil Hendricks <pfhendri1@gmail.com>
2026-02-27 15:15:57 -0600

    reset static defaults

--- mist-engine-sheets.js
+++ mist-engine-sheets.js
@@ -39,10 +39,46 @@ Hooks.once("ready", () => {
         }
     }
 
-    static get defaultOptions() {
-      return foundry.utils.mergeObject(super.defaultOptions, {
-        classes: ["otherscape"],
-      });
+    static DEFAULT_OPTIONS = {
+        classes: ['mist-engine', 'sheet', 'actor', 'otherscape-character'],
+        tag: 'form',
+        position: {
+            width: 1100,
+            height: 800
+        },
+        actions: {
+            createBackpackItem: this.#handleCreateBackpackItem,
+            deleteBackpackItem: this.#handleDeleteBackpackItem,
+            clickRoll: this.#handleClickRoll,
+            toggleToBurn: this.#handleToggleToBurn,
+            createQuintessence: this.#handleCreateQuintessence,
+            deleteQuintessence: this.#handleDeleteQuintessence,
+            createFellowship: this.#handleCreateFellowship,
+            deleteFellowship: this.#handleDeleteFellowship,
+            removeFellowshipThemecard: this.#handleRemoveFellowshipThemecard,
+            assignFellowshipThemecard: this.#handleAssignFellowshipThemecard,
+            clickedCustomBackground: this.#handleClickedCustomBackground,
+            clickedRemoveCustomBackground: this.#handleRemoveCustomBackground,
+            clickedCustomBackgroundEditor: this.#handleClickedCustomBackgroundEditor,
+            openThemekitSelection: this.#handleOpenThemekitSelection,
+            openThemekitCharacterApp: this.#handleOpenThemekitCharacterApp,
+            removeThemekit: this.#handleRemoveThemekit
+        },
+        form: {
+            submitOnChange: true
+        },
+        actor: {
+            type: 'character'
+        },
+        dragDrop: [{
+            dragSelector: '[draggable="true"]',
+            dropSelector: '.mist-engine.actor'
+        }],
+        window: {
+            resizable: true,
+            controls: [
+            ]
+        }
     }
   }
 
